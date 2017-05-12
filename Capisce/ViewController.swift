f5c4b47eaa23a76a3f244d500afb1fef3c41579a
Kelly Lougheed <kellylougheed@gmail.com>
2017-05-11 22:06:36 -0700

    Add activity indicator

--- Capisce/ViewController.swift
+++ Capisce/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var meaning: UILabel!
     @IBOutlet weak var parsing: UITextView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         var query: String = ""
@@ -25,9 +26,10 @@ class ViewController: UIViewController {
         let searchURL = "https://glosbe.com/gapi/translate?from=it&dest=eng&format=json&phrase=\(query)"
         
         let url = URL(string: searchURL)
-
-        meaning.isHidden = false
-        parsing.isHidden = false
+        
+        self.meaning.isHidden = true
+        self.parsing.isHidden = true
+        self.spinner.isHidden = false
         
         let task = URLSession.shared.dataTask(with: url!) { data, response, error in
             guard error == nil else {
@@ -43,18 +45,21 @@ class ViewController: UIViewController {
             if let json = json {
                 print(json)
                 
-                var meaningFound = false
-                var grammarFound = false
+                var wordFound = false
                 
                 // Unwrap JSON
                 if let meaningResult = (json as AnyObject)["tuc"] {
+                    
+                    // TODO: Try catch with OOB error
                     if let meaningResult2 = (meaningResult as AnyObject)[0] {
                         
                         // Find primary meaning
                         if let meaningResult3 = (meaningResult2 as AnyObject)["phrase"] {
                             if let meaningResult4 = (meaningResult3 as AnyObject)["text"] {
+                                self.spinner.isHidden = true
+                                self.meaning.isHidden = false
                                 self.meaning.text = meaningResult4 as? String
-                                meaningFound = true
+                                wordFound = true
                             }
                         }
                         
@@ -65,23 +70,22 @@ class ViewController: UIViewController {
                                 // Strip HTML tags
                                 str = str.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
                                 self.parsing.text = str
-                                grammarFound = true
+                                self.parsing.isHidden = false
                             }
                         }
                     }
                 }
                 
-                
-                // Check if results were found
-                if meaningFound == false {
+                if wordFound == false {
+                    self.spinner.isHidden = true
                     self.meaning.text = "No results found."
-                    self.parsing.text = "Please make sure that you have entered an Italian word."
+                    self.meaning.isHidden = false
+                    self.parsing.text = "Please search a different Italian word."
+                    self.parsing.isHidden = false
                 }
-                else if grammarFound == false {
-                    self.parsing.isHidden = true
-                }
-                
+            
             } else {
+                self.spinner.isHidden = true
                 self.meaning.text = "Unable to retrieve data."
             }
             
@@ -94,6 +98,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        spinner.isHidden = true
         meaning.isHidden = true
         parsing.isHidden = true
     }
